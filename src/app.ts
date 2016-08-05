82640836e66c415dd5eed61ee848ec4063333f17
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-08-05 23:57:34 +1000

    feat(app): single board and routing

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,25 @@ import {Aurelia, autoinject} from 'aurelia-framework';
 import {Router, RouterConfiguration} from 'aurelia-router';
 
 export class App {
+    router: Router;
+    
     configureRouter(config: RouterConfiguration, router: Router) {
-        config.title = 'Built With Aurelia';
+        config.title = 'Trello Clone';
 
         config.map([
             {
                 route: '', 
                 moduleId: './home',
-                name: 'hone',        
+                name: 'home',        
                 nav: true, 
                 title: 'Home'
+            },
+            {
+                route: 'b/:id/:name', 
+                moduleId: './board',
+                name: 'board',        
+                nav: false, 
+                title: 'Board'
             }
         ]);
 
